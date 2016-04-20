a90d935bb687a70c6ce81493f250e2b11e5167da
jaredwray <jaredwray@gmail.com>
2016-04-20 10:02:33 -0700

    adding in feed handling of providers

--- providers/atom.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var atom = function() {
-
-};
-
-modules.export = atom;
--- services/feed.js
+++ services/feed.js
@@ -1,6 +1,9 @@
 "use strict";
 
 var b = require('bluebird');
+var provider = require('./provider');
+var cache = require('./cache');
+
 
 var feed = function(config) {
     this.config = config;
@@ -9,7 +12,28 @@ var feed = function(config) {
 feed.prototype.get = function(count, callback) {
     var d = b.pending();
 
+    var cacheKey = cache.generateKey(count, "feed-get");
 
+    cache.get(cacheKey).then(function(data) {
+        d.resolve(data);
+    }).fail(function(error) {
+        var providers = [];
+
+        this.config.providers.forEach(function(p) {
+            providers.push(provider.create(p.name, p.options).get(count));
+        });
+
+        d.all(providers).then(function(result) {
+
+            var resultList = [];
+            result.forEach(function(r) {
+                resultList.push(r);
+            });
+
+            //TODO: sort the array
+        });
+
+    });
 
     d.asCallback(callback);
     return d.promise;
