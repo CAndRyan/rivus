5ab6b6ebe552ef9360afaeb3bd4c47cba716a106
jaredwray <jaredwray@gmail.com>
2016-05-03 22:05:18 -0700

    adding in RSS

--- providers/rss.js
+++ providers/rss.js
@@ -1,7 +1,17 @@
 "use strict";
 
-var rss = function() {
+var b = require('bluebird');
 
+var rss = function(config) {
+    this.config = config;
+};
+
+rss.prototype.get = function(count, callback) {
+    var d = b.pending();
+
+    d.reject(new Error("Not Implemented"));
+
+    return d.promise;
 };
 
 modules.export = rss;
