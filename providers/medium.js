0d4b2d1042adcf383497a25e9a69866b55c27341
jaredwray <jaredwray@gmail.com>
2016-05-24 12:12:06 -0700

    adding in medium provider

--- providers/medium.js
+++ providers/medium.js
@@ -1,7 +1,24 @@
-"use strict";
+'use strict';
 
-var medium = function() {
+var b = require('bluebird');
 
+var medium = function(config) {
+    this.setConfig(config);
+    this.ID = 'medium';
+};
+
+medium.prototype.get = function(count, callback) {
+    var d = b.pending();
+
+    //TODO: put the guts in here
+
+    d.asCallback(callback);
+    return d.promise;
+};
+
+medium.prototype.setConfig = function(config) {
+    this.config = config;
+    this.pc = config.getProvider('medium');
 };
 
 modules.export = medium;
